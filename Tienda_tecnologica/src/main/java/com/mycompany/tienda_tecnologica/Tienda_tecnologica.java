72718bfefd7f73623b0b7c847bd00f01a0e9d257
Lucas <lucberfer@gmail.com>
2024-11-27 19:12:10 +0100

    Test

---
+++ Tienda_tecnologica/src/main/java/com/mycompany/tienda_tecnologica/Tienda_tecnologica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tienda_tecnologica;
+
+/**
+ *
+ * @author lucas
+ */
+public class Tienda_tecnologica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
